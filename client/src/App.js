008ebaf4df792fda5b05476049f2bc7d766669d0
SzaboBence11 <szabo.bence0516+1@gmail.com>
2025-12-01 12:20:59 +0100

    added responsivity to one of the windows in wpf

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div class="flex-col mt-5">
       {backendData.communities.length > 0 ? (
         backendData.communities.map((community, i) => (
-          <div key={i} class="mx-auto border-blue-300 border-2 w-32 p-1 bg-slate-600 rounded-lg mb-1">
+          <div key={i} class="mx-auto border-blue-300 border-2 w-32 p-1 bg-slate-300 rounded-lg mb-1">
             <h3 class="text-center text-blue-200">{community.name}</h3>
           </div>
         ))
