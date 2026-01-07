e5206abbe20a76aa580c9d3b8dd009624e7be71d
SzaboBence11 <szabo.bence0516+1@gmail.com>
2026-01-07 09:06:43 +0100

    FRONTEND fixed sidebar and frontpage, folder structure rework

--- client/src/pages/FrontPage.js
+++ client/src/pages/FrontPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function FrontPage() {
+function FrontPage({ isSidebarOpen }) {
     const [backendData, setBackendData] = useState({ posts: [] })
 
     // Fetch random posts for the home page
@@ -18,15 +18,15 @@ function FrontPage() {
     return (
 
         // The whole frontpage area
-        <div className="p-4 sm:ml-64">
+        <div className={`p-4 ${isSidebarOpen ? "ml-64": "ml-16"}`}>
             <div className="p-4 border-1 rounded-base">
                 {/* Make space for each post */}
                 {backendData.posts.length > 0 ? (
                     backendData.posts.map((post, i) => (
 
                         // Post card
-                        <div className="flex flex-col items-center h-52 border shadow-md
-                                        mb-4 rounded-lg border-gray-400 p-3"
+                        <div className="flex flex-col items-center border shadow-md
+                                        mb-4 rounded-lg border-gray-400 p-3 max-h-100 min-h-52"
                              key={i}>
                             
                             {/* Post text content */}
@@ -36,10 +36,10 @@ function FrontPage() {
                             <p className='text-blue-950 text-2x1'>
                                 by: {post.poster_user} in {post.community}
                             </p>
-                            <p className='text-blue-950 mt-3'>
+                            <p className='text-blue-950 mt-3 overflow-hidden'>
                                 {post.post_text}
                             </p>
-                            <p className='text-blue-950 mt-3'>
+                            <p className='text-blue-950 mt-auto'>
                                 Posted at: {post.post_date}
                             </p>
                         </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,13 +3,14 @@ import Sidebar from './common/Sidebar';
 import FrontPage from './FrontPage';
 
 function Home() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
     return (
         <div>
 
             {/* Link sidebar and Frontpage together */}
-            <Sidebar />
-            <FrontPage />
+            <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
+            <FrontPage isSidebarOpen={isSidebarOpen}/>
         </div>
     )
 }
