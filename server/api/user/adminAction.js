46a21b048d31a8abe9191bd823599c3ea2e8efee
BertaBarnabas2006 <bertabarnabas2006@gmail.com>
2026-02-26 21:56:48 +0100

    Comment: Admin Action

--- server/api/user/adminAction.js
+++ server/api/user/adminAction.js
@@ -3,22 +3,27 @@ import express from 'express';
 
 const router = express.Router()
 
-// Get random communities (For Sidebar)
+// Execute requested admin action
 router.post('/adminAction', (req, res) => {
     try {
 
         // Get fetch data
         let args = req.body;
+
+        // Define sql with empty string value
         let sql = "";
 
-        console.log(args)
+        // User block,
+        // Invalidate user posts, communities, comments
         if(args.action == 'user'){
-            // Insert community join sql
+
+            // Set blocking sql
             sql = `
                 UPDATE users
                 SET blocked = 1
                 WHERE id = ?`
 
+            // Execute user block
             db.query(sql,[args.user_id],
                 (err, res1) => {
 
@@ -27,56 +32,38 @@ router.post('/adminAction', (req, res) => {
                     return res.status(400)
                             .json({ error: err });
                 
+                // Set user posts invalidating sql
                 sql = `
                     UPDATE posts
                     SET valid = 0
                     WHERE user_id = ?`
 
+                // Execute user posts invalidating
                 db.query(sql, [args.user_id],
                     (err, res2) => {
+
+                        // If there's an error
                         if (err)
                             return res.status(400)
                                       .json({ error: err });
 
+                        // Set user comments invalidating sql
                         sql = `
                             UPDATE comments
                             SET valid = 0
                             WHERE user_id = ?`
 
+                        // Execute user comments invalidating
                         db.query(sql,[args.user_id],
                             (err, res3) => {
+
+                                // If there's an error
                                 if (err)
                                     return res.status(400)
                                               .json({ error: err });
 
-                                sql = `
-                                    SELECT id
-                                    FROM posts
-                                    WHERE user_id = ?`
-                        
-                                db.query(sql, [args.user_id],
-                                    (err, res4) => {
-                                        if (err)
-                                            return res.status(400)
-                                                    .json({ error: err });
-
-                                            for(let i = 0; i < res4.length; i++){
-                                                sql = `
-                                                    UPDATE comments
-                                                    SET valid = 0
-                                                    WHERE user_id = ?`
-                                                
-                                                db.query(sql, [res4[i].id],
-                                                    (err, res5) => {
-                                                        if (err)
-                                                            return res.status(400)
-                                                                      .json({ error: err });
-                                                    }
-                                                )
-                                            }
-                                            res.json({res3});
-                                    }
-                                )
+                                // Return results to Frontend
+                                res.json(res3)
                             }
                         )
                     }
@@ -84,30 +71,40 @@ router.post('/adminAction', (req, res) => {
             });
         }
 
+        // User posts invalidating
         else if(args.action == 'post'){
+
+            // Set user posts invalidating sql
             sql = `
                 UPDATE posts
                 SET valid = 0
                 WHERE id = ?`
 
+            // Execute user posts invalidating
             db.query(sql, [args.post_id],
                 (err, res1) => {
 
+                    // If there's an error
                     if (err)
                         return res.status(400)
                                   .json({ error: err });
 
+                    // Post comments invalidating sql
                     sql = `
                         UPDATE comments
                         SET valid = 0
                         WHERE post_id = ?`
 
+                    // Execute post comments invalidating
                     db.query(sql, [args.post_id],
                         (err, res2) => {
+
+                            // If there's an error
                             if (err)
                                 return res.status(400)
                                           .json({ error: err });
                             
+                            // Return results to Frontend     
                             res.json(res2);
                         }
                     )
@@ -116,17 +113,25 @@ router.post('/adminAction', (req, res) => {
             )
         }
 
+        // Community invalidating
         else if(args.action == "community"){
+
+            // Set community invalidating sql
             sql = `
                 UPDATE communities
                 SET valid = 0
                 WHERE id = ?`
 
+            // Execute communiti invalidating
             db.query(sql, [args.community_id],
                 (err, res1) => {
+
+                    // If there's an error
                     if (err)
                         return res.status(400)
                                 .json({ error: err });
+
+                    // Return results to Frontend  
                     res.json(res1);
                 }
             )
@@ -134,6 +139,7 @@ router.post('/adminAction', (req, res) => {
 
     }
     catch (err) {
+        // Server error
         res.status(500).json("Szerver hiba!");
     }
 });
